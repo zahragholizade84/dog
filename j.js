edfeaac8185a64a2364734015d0d9916fbd57ed1
zahra gholizade <zhragholizade84>
2026-06-09 06:28:26 -0700

    first commit

---
+++ j.js
@@ -0,0 +1,10 @@
+fetch("https://api.palcoffee.ir/dogs/fact")
+.then(res => res.json())
+.then(data =>{
+    document.getElementById("result").innerText
+    = data.fact;
+})
+.catch(err =>{
+    document.getElementById("result").innerText
+    ="eroor";
+})
