faefd56a3e6035487b389a7a73ad563805286f09
zahra gholizade <zhragholizade84>
2026-06-13 15:58:02 -0700

    js is true

--- j.js
+++ j.js
@@ -1,10 +1,10 @@
 fetch("https://api.palcoffee.ir/dogs/fact")
 .then(res => res.json())
-.then(data =>{
-    document.getElementById("result").innerText
-    = data.fact;
-})
-.catch(err =>{
-    document.getElementById("result").innerText
-    ="eroor";
-})
+.then(data =>
+    document.getElementById("result").innerText =
+    data.fact
+)
+.catch(err =>
+    document.getElementById("result").innerText =
+    undefined
+)
