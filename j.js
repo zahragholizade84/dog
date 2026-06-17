833b0ec163ffe94805e03e44a52c09592922f0a9
zahra gholizade <zhragholizade84>
2026-06-17 02:14:10 -0700

    fix api img

--- j.js
+++ j.js
@@ -1,19 +1,34 @@
-fetch("https://api.palcoffee.ir/dogs/fact")
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-    document.getElementById("result").innerText =
-        JSON.stringify(data);
-})
-.catch(err => {
+const img = document.getElementById("img");
+img.addEventListener("load", () => {
+  document.getElementById("loder").classList.add("hidden");
+  document.getElementById("img").classList.remove("hidden");
+}
 
-    document.getElementById("result").innerText =
+)
+
+function getdog() {
+  document.getElementById("loder").classList.remove("hidden");
+  document.getElementById("img").classList.add("hidden");
+  fetch("https://random.dog/woof.json")
+    .then(res => res.json())
+    .then(data => {
+      img.src = data.url;
+    })
+    .catch(err => console.error(err))
+
+  fetch("https://api.palcoffee.ir/dogs/fact")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      document.getElementById("result").innerText =
+        JSON.stringify(data);
+    })
+    .catch(err => {
+      document.getElementById("result").innerText =
         "ERROR";
-});
-//fetch('https://dog.ceo/api/breeds/image/random')
- // .then(res => res.json())
-  //.then(data => {
-  //  console.log(data.message); // لینک عکس
-   // document.getElementById("img").src =
-   // data.message;
- // });
+    });
+
+}
+getdog();
+document.getElementById("btn").addEventListener("click", getdog);
+
